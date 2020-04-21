57cba1603ffec815563e18b85d7af7c91be98bb3
K. Joeri van der Velde <joeriv@gmail.com>
2020-04-21 16:41:51 +0200

    Added doc to import script

--- fairgenomes_molgenis_app/data/addNullFlavorsAndImportCodebook.sh
+++ fairgenomes_molgenis_app/data/addNullFlavorsAndImportCodebook.sh
@@ -1,3 +1,7 @@
+# concatenates HL7v3 NullFlavors to a codebook file and import
+# usage: supply a single argument that will be used as the target codebook
+# target codebook should be 'fair-genomes' package
+# see: https://www.hl7.org/fhir/v3/NullFlavor/cs.html
 cat $1.tsv hl7v3_nullflavors.tsv > $1_with_nf.tsv
 mcmd import -p $1_with_nf.tsv --as fair-genomes_$1 --in fair-genomes
 rm $1_with_nf.tsv
